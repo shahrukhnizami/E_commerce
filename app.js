945d1d97cad0fffbcdd3a8658e767a7431456f61
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-07-24 07:34:31 +0500

    aa

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ const register_name = document.getElementById("register_name")
 const register_image = document.getElementById("register_image")
 const registerbtn_login = document.getElementById("registerbtn_login")
 // console.log(register_user);
-logOut_btn.style.display="none"
+// logOut_btn.style.display="none"
 onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log("User Is Login");
         signinbtn_login.style.display="none"
         registerbtn_login.style.display="none"
+        logOut_btn.style.display="block"
         // register_name.innerText= register_name;      //   window.location.href = "index.html"
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/auth.user
@@ -30,6 +31,8 @@ onAuthStateChanged(auth, (user) => {
     } else {
         console.log("User is not Login");
         logOut_btn.style.display="none"
+        signinbtn_login.style.display="block"
+        registerbtn_login.style.display="block"
       // User is signed out
       // ...
     }
